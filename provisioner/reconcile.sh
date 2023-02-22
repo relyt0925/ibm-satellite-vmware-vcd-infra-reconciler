6da6c2fb16537ae4332fd3ad2f6e4e0b214f0f6c
Tyler Lisowski <lisowski@us.ibm.com>
2023-02-21 23:12:25 -0600

    up

--- provisioner/reconcile.sh
+++ provisioner/reconcile.sh
@@ -2,7 +2,7 @@
 # ASSUMES LOGGED INTO APPROPRIATE IBM CLOUD ACCOUNT: TO DO THAT AUTOMATICALLY
 # ibmcloud login -a https://cloud.ibm.com --apikey XXXX -r us-south
 set +x
-source secrets.env
+source config.env
 set -x
 export LOCATION_ID=vmware-location-demo-3
 core_machinegroup_reconcile() {
@@ -13,26 +13,29 @@ core_machinegroup_reconcile() {
 		return
 	fi
 	if ! grep "success" "$INSTANCE_DATA"; then
-	  return
+		return
 	fi
 	VAPP_FILTER_PREFIX=$(echo "$HOST_LABELS" | awk -F '=' '{print $2}')
-	TOTAL_INSTANCES=$(grep "$VAPP_FILTER_PREFIX"  "$INSTANCE_DATA" | wc -l)
+	TOTAL_INSTANCES=$(grep "$VAPP_FILTER_PREFIX" "$INSTANCE_DATA" | wc -l)
 	if ((COUNT > TOTAL_INSTANCES)); then
 		NUMBER_TO_SCALE=$((COUNT - TOTAL_INSTANCES))
-		IGN_FILE_PATH=$(bx sat host attach --location "$LOCATION_ID" --operating-system "RHCOS" --host-label "$HOST_LABELS" | grep "register-host")
+		if [[ -n "$HOST_LINK_AGENT_ENDPOINT" ]]; then
+			IGN_FILE_PATH=$(bx sat host attach --location "$LOCATION_ID" --operating-system "RHCOS" --host-label "$HOST_LABELS" --host-link-agent-endpoint "$HOST_LINK_AGENT_ENDPOINT" | grep "register-host")
+		else
+			IGN_FILE_PATH=$(bx sat host attach --location "$LOCATION_ID" --operating-system "RHCOS" --host-label "$HOST_LABELS" | grep "register-host")
+		fi
 		if [[ "$IGN_FILE_PATH" != *".ign" ]]; then
 			continue
 		fi
-		for i in $(seq 1 $NUMBER_TO_SCALE)
-    do
-       export VAPP_NAME="${VAPP_FILTER_PREFIX}-$(date +%s)"
-       export VAPP_NAME_64=$(gprintf $VAPP_NAME | base64 -w 0)
-       export IGN_FILE_PATH_WITH_HOSTNAME_64=/tmp/ignitionwithhostname.b64
-       jq --arg injecthost "$VAPP_NAME_64" '.storage.files = [{"path": "/etc/hostname", "mode": 420, "contents": { "source": ("data:text/plain;charset=utf-8;base64," + $injecthost) }}] + .storage.files' "$IGN_FILE_PATH"  | base64 -w 0 > "$IGN_FILE_PATH_WITH_HOSTNAME_64"
-       if ! pwsh -f create_machine.ps1; then
-       		continue
-       	fi
-    done
+		for i in $(seq 1 $NUMBER_TO_SCALE); do
+			export VAPP_NAME="${VAPP_FILTER_PREFIX}-$(date +%s)"
+			export VAPP_NAME_64=$(gprintf $VAPP_NAME | base64 -w 0)
+			export IGN_FILE_PATH_WITH_HOSTNAME_64=/tmp/ignitionwithhostname.b64
+			jq --arg injecthost "$VAPP_NAME_64" '.storage.files = [{"path": "/etc/hostname", "mode": 420, "contents": { "source": ("data:text/plain;charset=utf-8;base64," + $injecthost) }}] + .storage.files' "$IGN_FILE_PATH" | base64 -w 0 >"$IGN_FILE_PATH_WITH_HOSTNAME_64"
+			if ! pwsh -f create_machine.ps1; then
+				continue
+			fi
+		done
 	fi
 }
 
@@ -46,9 +49,9 @@ remove_dead_machines() {
 		NAME=$(_jq '.name')
 		if [[ "$HEALTH_STATE" == "reload-required" ]]; then
 			export VAPP_NAME="$NAME"
-      if ! pwsh -f delete_infra.ps1; then
-        continue
-      fi
+			if ! pwsh -f delete_infra.ps1; then
+				continue
+			fi
 			ibmcloud sat host rm --location "$LOCATION_ID" --host "$NAME" -f
 		fi
 	done
@@ -61,11 +64,11 @@ while true; do
 	export HOSTS_DATA_FILE=/tmp/${LOCATION_ID}-hosts-data.txt
 	export SERVICES_DATA_FILE=/tmp/${LOCATION_ID}-services-data.txt
 	if ! bx sat locations >$LOCATION_LIST_FILE; then
-  		continue
-  fi
-  if ! grep "$LOCATION_ID" /tmp/location-lists.txt; then
-    bx sat location create --name "$LOCATION_ID" --coreos-enabled --managed-from wdc
-  fi
+		continue
+	fi
+	if ! grep "$LOCATION_ID" /tmp/location-lists.txt; then
+		bx sat location create --name "$LOCATION_ID" --coreos-enabled --managed-from wdc
+	fi
 	if ! bx sat hosts --location $LOCATION_ID --output json >$HOSTS_DATA_FILE; then
 		continue
 	fi
